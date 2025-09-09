91cab7b1419585e387fecf7d4c8bf6f461f4221b
Diego Puerta <d.puertamartin97@gmail.com>
2025-09-09 11:12:08 +0200

    new querys

--- sql/querys.sql
+++ sql/querys.sql
@@ -12,3 +12,15 @@ GO
 
 EXEC dbo.sp_load_fact_sales;
 GO
+SELECT COUNT(*) AS filas_customers FROM stg.customers;
+SELECT COUNT(*) AS filas_products FROM stg.products;
+SELECT COUNT(*) AS filas_orders FROM stg.orders;
+
+SELECT TOP 10 * FROM stg.customers;
+SELECT TOP 10 * FROM stg.products;
+SELECT TOP 10 * FROM stg.orders;
+
+SELECT COUNT(*) AS filas_fact_sales FROM dbo.fact_sales;
+SELECT TOP 10 * FROM dbo.fact_sales ORDER BY sales_key DESC;
+SELECT TOP 10 * FROM dbo.dim_product;
+SELECT TOP 10 * FROM dbo.dim_customer;
